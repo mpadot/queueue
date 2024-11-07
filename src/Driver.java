79882149b931b27820f90388dadad771e722c76a
Miles Padot <155395178+mpadot@users.noreply.github.com>
2024-11-07 00:12:23 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Driver {
+    public static void main(String[] args) {
+        queue queueSLL = new queue();
+        queueSLL.enqueue(10);
+        queueSLL.enqueue(20);
+        queueSLL.enqueue(30);
+        queueSLL.print();
+        System.out.println();
+        queueSLL.dequeue();
+        queueSLL.print();
+
+        System.out.println(queueSLL.peek());
+
+    }
 }
+
